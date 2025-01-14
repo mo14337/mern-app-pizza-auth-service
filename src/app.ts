2ceed5fb1961a217dda41e41e052de174549275d
mo14337 <mohitsingh14336@gmail.com>
2025-01-14 22:39:00 +0530

    add docker

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import logger from './config/logger';
 import { HttpError } from 'http-errors';
+import AuthRouter from './routes/auth';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('hello');
-});
+app.use('/auth', AuthRouter);
+
 //global error handler.
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
