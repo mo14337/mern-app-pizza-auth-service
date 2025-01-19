ee1e4a8a71fcb68c35b82ff5a2250adb35f3a36f
mo14337 <mohitsingh14336@gmail.com>
2025-01-19 16:13:21 +0530

    Add key generation script

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,6 +1,6 @@
-import crypto from "crypto";
-import fs from "fs";
-import path from "path";
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
 
 // Get the path to the mern-app-pizza-auth-service directory
 const authServiceDir = path.dirname(new URL(import.meta.url).pathname); // Get the current directory of the script
@@ -10,24 +10,24 @@ const certsDir = path.join(authServiceDir, '..', 'certs'); // Move one directory
 
 // Ensure the certs directory exists
 if (!fs.existsSync(certsDir)) {
-  fs.mkdirSync(certsDir);
+    fs.mkdirSync(certsDir);
 }
 
-const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-  modulusLength: 2048,
-  publicKeyEncoding: {
-    type: "pkcs1",
-    format: "pem"
-  },
-  privateKeyEncoding: {
-    type: "pkcs1",
-    format: "pem"
-  }
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem',
+    },
+    privateKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem',
+    },
 });
 
-console.log("publicKey", publicKey);
-console.log("privateKey", privateKey);
+console.log('publicKey', publicKey);
+console.log('privateKey', privateKey);
 
 // Write the private and public keys to the certs folder inside mern-app-pizza-auth-service
-fs.writeFileSync(path.join(certsDir, "private.pem"), privateKey);
-fs.writeFileSync(path.join(certsDir, "public.pem"), publicKey);
+fs.writeFileSync(path.join(certsDir, 'private.pem'), privateKey);
+fs.writeFileSync(path.join(certsDir, 'public.pem'), publicKey);
