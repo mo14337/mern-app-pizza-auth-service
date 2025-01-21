a849b58c58ab8cb8d44d958afac41cf04e2e5746
mo14337 <mohitsingh14336@gmail.com>
2025-01-21 23:04:05 +0530

    Added database connection

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
 import app from './app';
 import { Config } from './config';
+import { AppDataSource } from './config/data-source';
 import logger from './config/logger';
 
-const startServer = () => {
+const startServer = async () => {
     const PORT = Config.PORT;
     try {
+        await AppDataSource.initialize();
+        logger.info('Database connected');
         app.listen(PORT, () => {
             logger.info('listing on port', { port: PORT });
         });
-    } catch (error) {
-        console.error(error);
-        process.exit();
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
-startServer();
+void startServer();
